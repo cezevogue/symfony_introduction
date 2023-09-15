80fe2043d16f95685d7525e97f15cb1337e6dd9f
cezevogue <cezdesaulle.evogue@gmail.com>
2023-09-15 10:38:00 +0200

    request

--- src/Controller/HttpController.php
+++ src/Controller/HttpController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,18 +17,55 @@ class HttpController extends AbstractController
             'controller_name' => 'HttpController',
         ]);
     }
-    
-    
-      #[Route('/request', name: 'request')]
-          public function request(): Response
-          {
-             
-               
-              return $this->render('http/request.html.twig', [
-                 
-              ]);
-          }
 
+
+    #[Route('/request', name: 'request')]   // ici on injecte en dépendance la classe Request de HttpFoundation instanciée dans l'objet $request
+    public function request(Request $request): Response
+    {
+
+
+        // Request permet de récupérer les données provenant des superglobale (principalement pour $_POST, $_GET, $_COOKIES)
+
+        //http://localhost:8000/request?nom=desaulle&prenom=cesaire
+        dump($_GET);
+
+        dump($request);
+
+          // $request->query contient un objet qui est une surcouche de $_GET
+
+
+        //  var_dump($_GET)
+        //dd($request->query->all());
+
+        // echo $_GET['prenom']
+        echo $request->query->get('prenom');
+
+        // isset($_GET['surnom'])
+        dump($request->query->has('surnom')); // false
+
+        dump($request->query->get('surnom')); // null sans erreur
+
+        dump($request->query->get('surnom', 'toto'));//  toto car déclaré comme valeur par defaut
+        //echo $_GET['surnom'];  warning:undefined variable surnom
+
+        dump($request->getMethod());// renvoie la méthode utilisée
+
+        // si on est en méthode post le code suivant s'execute
+        if ($request->isMethod('POST')){
+
+            dump($request->request->all());
+        }
+
+        // ces mêmes méthodes s'appliquent de même sur l'objet $request->request qui contient une surcouche de $_POST
+
+
+
+
+
+        return $this->render('http/request.html.twig', [
+
+        ]);
+    }
 
 
 
